4d4f19c2553a7c505513310da6203ed9a8da1006
Radoslav Yovchev <radoslav.tm@gmail.com>
2018-02-15 21:31:39 +0200

    Fixed some CLI build issues. Automated even more the Boost dependency.

--- tools/build_boost.bat
+++ tools/build_boost.bat
@@ -1,12 +1,39 @@
 @echo off
-REM https://sourceforge.net/projects/boost/files/boost/1.66.0/
-REM https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.7z
 
-pushd "%~dp0\..\dep\boost\"
-if %errorlevel% neq 0 (
-    echo Unable to locate the Boost folder
-    exit /b 1
+set BOOST_ROOT="%~dp0\..\dep\boost"
+
+REM Zero the error level
+ver > nul
+
+if not exist "%BOOST_ROOT%\bootstrap.bat" (
+
+    echo Download Boost...
+    REM https://sourceforge.net/projects/boost/files/boost/1.66.0/
+    REM https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.7z
+    python3 "%~dp0\download.py" ^
+        https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.7z ^
+        "%~dp0\boost.7z"
+    if %errorlevel% neq 0 (
+        echo Unable to download Boost, exiting...
+        exit /b 1
+    )
+
+    echo Rename Boost...
+    "%~dp0\7za.exe" rn "%~dp0\boost.7z" boost_1_66_0 boost
+    if %errorlevel% neq 0 (
+        echo Unable to rename Boost, exiting...
+        exit /b 1
+    )
+
+    echo Unpack Boost...
+    "%~dp0\7za.exe" -aoa -spe x "%~dp0\boost.7z" -o"%BOOST_ROOT%"
+    if %errorlevel% neq 0 (
+        echo Unable to unpack Boost, exiting...
+        exit /b 1
+    )
 )
+
+pushd "%BOOST_ROOT%"
 
 if not exist b2.exe ( 
     echo b2 not found, bootstrapping Boost...
@@ -18,7 +45,10 @@ if not exist b2.exe (
     )
 )
 
-b2 variant=release,debug link=static threading=multi runtime-link=static address-model=64 --with-serialization --with-date_time --with-filesystem --with-thread --build-dir=garbage --stagedir=boost_lib stage
+b2 variant=release,debug link=static threading=multi runtime-link=static address-model=64 ^
+ --with-serialization --with-date_time --with-filesystem --with-thread ^
+ --build-dir=garbage --stagedir=boost_lib stage
+
 if %errorlevel% neq 0 (
     echo Boost build failed, exiting...
     popd
