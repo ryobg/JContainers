d8aa0cbd01b0153dc59b9b8e53c0e6b2c79fc59b
ryobg <ryobg@users.noreply.github.com>
2018-02-15 21:55:46 +0200

    Where are thou Python? Fixed the error handling in the boost batch too

--- tools/build_boost.bat
+++ tools/build_boost.bat
@@ -13,21 +13,21 @@ if not exist "%BOOST_ROOT%\bootstrap.bat" (
     python3 "%~dp0\download.py" ^
         https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.7z ^
         "%~dp0\boost.7z"
-    if %errorlevel% neq 0 (
+    if errorlevel 1 (
         echo Unable to download Boost, exiting...
         exit /b 1
     )
 
     echo Rename Boost...
     "%~dp0\7za.exe" rn "%~dp0\boost.7z" boost_1_66_0 boost
-    if %errorlevel% neq 0 (
+    if errorlevel 1 (
         echo Unable to rename Boost, exiting...
         exit /b 1
     )
 
     echo Unpack Boost...
     "%~dp0\7za.exe" -aoa -spe x "%~dp0\boost.7z" -o"%BOOST_ROOT%"
-    if %errorlevel% neq 0 (
+    if errorlevel 1 (
         echo Unable to unpack Boost, exiting...
         exit /b 1
     )
@@ -38,7 +38,7 @@ pushd "%BOOST_ROOT%"
 if not exist b2.exe ( 
     echo b2 not found, bootstrapping Boost...
     call bootstrap.bat
-    if %errorlevel% neq 0 (
+    if errorlevel 1 (
         echo Unable to bootstrap, exiting...
         popd
         exit /b 2
@@ -49,7 +49,7 @@ b2 variant=release,debug link=static threading=multi runtime-link=static address
  --with-serialization --with-date_time --with-filesystem --with-thread ^
  --build-dir=garbage --stagedir=boost_lib stage
 
-if %errorlevel% neq 0 (
+if errorlevel 1 (
     echo Boost build failed, exiting...
     popd
     exit /b 3
